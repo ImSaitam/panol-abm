953914236f1b1e88d7bd5f429276111ba54544a7
ImSaitam <matutedesanto@gmail.com>
2024-10-07 18:30:06 -0300

    tipo herramienta post

--- src/components/AñadirTipoBoton.js
+++ src/components/AñadirTipoBoton.js
@@ -1,25 +1,84 @@
-import React, { useState } from 'react';
-import { Button, Modal, Form } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../añadircategoriaboton.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../añadircategoriaboton.css";
+import axios from "axios";
 
 export default function AñadirTipoBoton() {
   const [show, setShow] = useState(false);
   const [formData, setFormData] = useState({
     nombre: "",
   });
+  const [categorias, setCategorias] = useState([]);
+  const [selectedCategoryId, setSelectedCategoryId] = useState(null);
+  const [selectedSubcategoryId, setSelectedSubcategoryId] = useState(null);
+  const [subcategorias, setSubcategorias] = useState([]);
+  const [filteredSubcategorias, setFilteredSubcategorias] = useState([]);
+
+  useEffect(() => {
+    const fetchCategorias = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/categorias");
+        setCategorias(response.data);
+      } catch (error) {
+        console.error("Error al obtener las categorías", error);
+      }
+    };
+
+    fetchCategorias();
+  }, []);
+
+  useEffect(() => {
+    const fetchSubcategorias = async () => {
+      if (selectedCategoryId) {
+        try {
+          const response = await axios.get(
+            `http://localhost:5000/subcategorias`
+          );
+          setSubcategorias(response.data);
+          setFilteredSubcategorias(
+            response.data.filter(
+              (subcategoria) =>
+                subcategoria.categoria_id === parseInt(selectedCategoryId)
+            )
+          );
+        } catch (error) {
+          console.error("Error al obtener las subcategorías", error);
+        }
+      } else {
+        setFilteredSubcategorias([]);
+      }
+    };
+
+    fetchSubcategorias();
+  }, [selectedCategoryId]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/tipo-herramienta",
+        {
+          ...formData,
+          categoria_id: selectedCategoryId,
+          subcategoria_id: selectedSubcategoryId,
+        },
+        {
+          "Content-Type": "application/json",
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
     <>
-      <Button
-        variant="secondary"
-        onClick={handleShow}
-        className="add-button"
-      >
+      <Button variant="secondary" onClick={handleShow} className="add-button">
         +
       </Button>
 
@@ -28,22 +87,52 @@ export default function AñadirTipoBoton() {
           <Modal.Title>Añadir tipo</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="formCategoryName">
               <Form.Label>Seleccione la categoría</Form.Label>
-              <Form.Select>
- 
+              <Form.Select
+                onChange={(event) => {
+                  console.log(event.target.value);
+                  setSelectedCategoryId(event.target.value);
+                  setFormData({ ...formData, categoriaId: event.target.value });
+                }}
+              >
+                <option value="">Seleccione la categoría</option>
+                {categorias.map((categoria) => (
+                  <option key={categoria.id} value={categoria.id}>
+                    {categoria.nombre}
+                  </option>
+                ))}
               </Form.Select>
               <Form.Label>Seleccione la subcategoría</Form.Label>
-              <Form.Select>
-                <option value="Subcategoría 1">Subcategoría 1</option>
+              <Form.Select
+                onChange={(event) => {
+                  setSelectedSubcategoryId(event.target.value);
+                  setFormData({
+                    ...formData,
+                    subcategoriaId: event.target.value,
+                  });
+                }}
+              >
+                <option value="">Seleccione la subcategoría</option>
+                {filteredSubcategorias.map((subcategoria) => (
+                  <option key={subcategoria.id} value={subcategoria.id}>
+                    {subcategoria.nombre}
+                  </option>
+                ))}
               </Form.Select>
               <Form.Label>Nombre de tipo</Form.Label>
-              <Form.Control type="text" placeholder="Ingresa el nombre del tipo" />
+              <Form.Control
+                type="text"
+                placeholder="Ingresa el nombre del tipo"
+                onChange={(event) =>
+                  setFormData({ ...formData, nombre: event.target.value })
+                }
+              />
             </Form.Group>
-            <Button type='submit' variant="primary" onClick={handleClose}>
-            Guardar
-          </Button>
+            <Button type="submit" variant="primary" onClick={handleClose}>
+              Guardar
+            </Button>
           </Form>
         </Modal.Body>
         <Modal.Footer>
